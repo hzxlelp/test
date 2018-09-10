816bb54a25fbba7ae63a959f314662a161721827
Peak <peakhu.mason@gmail.com>
2018-09-10 19:05:59 +0800

    xxx

--- testUITests/testUITests.swift
+++ testUITests/testUITests.swift
@@ -13,9 +13,8 @@ class testUITests: XCTestCase {
     override func setUp() {
         super.setUp()
         
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+        // Put setup code here. This method is called before the invocation of each test method in the class.        
+
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
